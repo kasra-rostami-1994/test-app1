f0ae491b38f27e66792445a9d533b0c8cc07035b
kasra-rostami-1994 <rkasra18@gmail.com>
2025-02-19 03:04:53 +0330

    final server to database

--- server.js
+++ server.js
@@ -3,23 +3,27 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// مسیرهای استاتیک
-app.use(express.static(path.join(__dirname, 'public')));
+// سرو کردن فایل‌های استاتیک از روت
+app.use(express.static(path.join(__dirname)));
 
 // اتصال به MongoDB
 const dbURI = process.env.DB_URI || 'mongodb+srv://rkasra18:920771018@cluster0.o5y10.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('Error connecting to MongoDB:', err));
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 // مسیر برای بررسی اتصال
 app.get('/check-db-connection', (req, res) => {
-  mongoose.connection.readyState === 1
-    ? res.json({ status: 'success' })
-    : res.json({ status: 'error' });
+  if (mongoose.connection.readyState === 1) {
+    res.json({ status: 'success' });
+  } else {
+    res.json({ status: 'error' });
+  }
 });
 
 // مسیر برای نمایش فایل HTML
@@ -30,9 +34,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
