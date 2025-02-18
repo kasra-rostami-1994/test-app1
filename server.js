48c6281a52202656bea3165877f367ef4e1d1921
kasra-rostami-1994 <rkasra18@gmail.com>
2025-02-19 02:51:35 +0330

    server update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();  // برای بارگزاری فایل .env
+
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb+srv://rkasra18:920771018@cluster0.o5y10.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const dbURI = process.env.DB_URI;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
