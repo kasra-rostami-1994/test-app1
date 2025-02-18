d24f97470d51cff7d57dda2778a0b77070d41675
kasra-rostami-1994 <rkasra18@gmail.com>
2025-02-19 02:56:27 +0330

    error

--- server.js
+++ server.js
@@ -30,3 +30,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
