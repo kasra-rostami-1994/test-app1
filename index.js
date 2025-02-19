cb7d2aecdcab9b47b20335ce4f91a56bbcdbe026
kasra-rostami-1994 <rkasra18@gmail.com>
2025-02-19 15:50:41 +0330

    patch_01-feb19

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect('mongodb+srv://rkasra18:920771018@cluster0.o5y10.mongodb.net/userdb')
+
+
+ const   UserSchema = new mongoose.Schema({
+
+     name:String,
+     age:Number
+ })
+
+ const UserModel = mongoose.model("emp", UserSchema);
+
+app.listen (3000, ()=>{
+    console.log('server is running.')
+})
+
