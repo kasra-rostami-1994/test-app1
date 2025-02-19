33843c1ebbfd194913f5680c932f12ffc5d8422e
kasra-rostami-1994 <rkasra18@gmail.com>
2025-02-19 14:24:35 +0330

    start add

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const UserModel = mongoose.model('User', UserSchema);
 
 // تنظیمات نمایش داده‌ها در صفحه
 app.set('view engine', 'ejs');  // استفاده از موتور نمایش ejs
-app.set('views', '');  // مسیر اصلی
+app.set('views', path.join(__dirname, 'views')); // دایرکتوری views
 
 // روت برای نمایش داده‌های کاربران
 app.get('/', async (req, res) => {
