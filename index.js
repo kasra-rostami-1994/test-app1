1a80a54086385aaaecf0127cf964c1ad7e2a74b4
kasra-rostami-1994 <rkasra18@gmail.com>
2025-02-19 13:54:30 +0330

    favicon script

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+
+console.log("Server is running...");
+
+
 mongoose.connect('mongodb+srv://rkasra18:920771018@cluster0.o5y10.mongodb.net/userdb')
     .then(() => {
         console.log('MongoDB connected');
     })
     .catch(err => {
         console.log('Error connecting to MongoDB:', err);
+    });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Mongoose is connected to the database');
+    });
+    
+    mongoose.connection.on('error', (err) => {
+        console.log('Mongoose connection error:', err);
+    });
+    
+    mongoose.connection.on('disconnected', () => {
+        console.log('Mongoose is disconnected');
     });
 
 // مسیر روت اصلی
@@ -27,4 +43,14 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+
+app.get('/userdb', async (req, res) => {
+    try {
+        const users = await UserModel.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: 'Error fetching data from database', error: err });
+    }
 });
