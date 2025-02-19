41f1dcb153fe9bbf867cee7301bc32fc957c60f8
kasra-rostami-1994 <rkasra18@gmail.com>
2025-02-19 16:14:42 +0330

    new data base adding test

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express()
 mongoose.connect('mongodb+srv://rkasra18:920771018@cluster0.o5y10.mongodb.net/userdb')
 
 
-//   const   UserSchema = new mongoose.Schema({
+   const   UserSchema = new mongoose.Schema({
 
-//       name:String,
-//      age:Number
-//   })
+       name:String,
+      age:Number
+   })
 
-//   const UserModel = mongoose.model("test-base-in-creation", UserSchema);
+   const UserModel = mongoose.model("test-base-in-creation", UserSchema);
 
 app.listen (3000, ()=>{
     console.log('server is running.')
