5f1443e237a3379047f05f5d6245ff574334e43c
kasra-rostami-1994 <rkasra18@gmail.com>
2025-02-19 13:35:32 +0330

    console log add

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express()
 
 mongoose.connect('mongodb+srv://rkasra18:920771018@cluster0.o5y10.mongodb.net/userdb')
 
+.then(() => {
+    console.log('MongoDB connected'); // پیام موفقیت در اتصال
+})
+.catch(err => {
+    console.log('Error connecting to MongoDB:', err); // پیام خطا در اتصال
+});
+
 
  const   UserSchema = new mongoose.Schema({
 
